c0d931e74bbf69026029e765664fb2ba440183f4
Timothy <timmytam10@gmail.com>
2024-12-14 10:27:17 +1100

    add responsive blog post and image update schema to content with imgs in content field

--- nextjs-blog/studio-sanity-blog/schemaTypes/blog.ts
+++ nextjs-blog/studio-sanity-blog/schemaTypes/blog.ts
@@ -1,40 +1,52 @@
 export default {
-    name: 'blog',
-    type: 'document',
-    title: 'Blog',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Title of blog article',
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title of blog article',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug of your blog article',
+      options: {
+        source: 'title',
       },
-      {
-        name: 'slug',
-        type: 'slug',
-        title: 'Slug of your blog article',
-        options: {
-          source: 'title',
-        },
-      },
-      {
-        name: 'titleImage',
-        type: 'image',
-        title: 'Title Image',
-      },
-      {
-        name: 'smallDescription',
-        type: 'text',
-        title: 'Small Description',
-      },
-      {
-        name: 'content',
-        type: 'array',
-        title: 'Content',
-        of: [
-          {
-            type: 'block',
+    },
+    {
+      name: 'titleImage',
+      type: 'image',
+      title: 'Title Image',
+    },
+    {
+      name: 'smallDescription',
+      type: 'text',
+      title: 'Small Description',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        { type: 'block' }, // For rich text content
+        { 
+          type: 'image',
+          options: {
+            hotspot: true, // Enable hotspot for cropping images
           },
-        ],
-      },
-    ],
-  }
+          fields: [
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative Text',
+              description: 'Describe the code screenshot for accessibility',
+            },
+          ],
+        }, 
+      ],
+    },
+  ],
+};
